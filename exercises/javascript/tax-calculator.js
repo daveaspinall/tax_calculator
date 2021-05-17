53827121397ddb65548a3114fa583a921836605c
Dave Aspinall <dave.aspinall@autotrader.co.uk>
2021-05-17 12:19:14 +0100

    chore: implement tax calculator on a petrol vehicle for the first year

--- exercises/javascript/tax-calculator.js
+++ exercises/javascript/tax-calculator.js
@@ -13,8 +13,38 @@ let TaxCalculator = class TaxCalculator {
   }
 
   calculateTax(vehicle) {
-    throw new Error('You have to implement the method doSomething!');
-  }
-}
+    const { co2Emissions } = vehicle;
 
-module.exports = { TaxCalculator: TaxCalculator }
+    if (co2Emissions === 0) {
+      return 0;
+    } else if (co2Emissions <= 50) {
+      return 10;
+    } else if (co2Emissions <= 75) {
+      return 25;
+    } else if (co2Emissions <= 90) {
+      return 105;
+    } else if (co2Emissions <= 100) {
+      return 125;
+    } else if (co2Emissions <= 110) {
+      return 145;
+    } else if (co2Emissions <= 130) {
+      return 165;
+    } else if (co2Emissions <= 150) {
+      return 205;
+    } else if (co2Emissions <= 170) {
+      return 515;
+    } else if (co2Emissions <= 190) {
+      return 830;
+    } else if (co2Emissions <= 225) {
+      return 1240;
+    } else if (co2Emissions <= 255) {
+      return 1760;
+    } else if (co2Emissions > 255) {
+      return 2070;
+    }
+
+    return 0;
+  }
+};
+
+module.exports = { TaxCalculator: TaxCalculator };
